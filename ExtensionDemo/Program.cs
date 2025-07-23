a03fd22274f7e59083edd0e2c14efc728ba3c6b3
Earl Syx Hernandez <hernandezearlsyxmail@gmail.com>
2025-07-24 06:28:52 +0800

    -Extension methods make it easier to read your code
    -easir to call things.
    - list of data to save in databse
    - cool if on your list, people.save . really simple.
    list type, .netframework.
    save database.
    easy to read. sesy to work with,as you create lbirary for these extension

---
+++ ExtensionDemo/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //create hotel room model
+            HotalRoomModel room = new HotalRoomModel();
+
+            room.TurnOnAir().SetTemparature(72).OpenShades();//chaining
+            //since each one return a hotel room model.
+            // the return alue of air is what we work on temp, and return value of temp is the one we work on openshades  
+            // since we return the same object we can do multiple things on it
+            //OpenShades(SetTemperateure(TurnOnAir(room)));
+
+            //known as fluent design - use to extend your own things or something you dont control, this method. , has o b static c lass,has to be static method, also this kewotd ciritcal, if it's dont have ti then it's just the moherod
+
+            room.TurnOffAir().CloseShades();
+            //readable et of fmethod calls.
+            "HelloWorld".PrintToConsole();
+            Console.ReadLine();
+        }
+    }
+
+    public class HotalRoomModel
+    {
+        public int Temperature { get; set; }
+        public bool IsAirRunning { get; set; }
+        public bool AreShadesOpen { get; set; }
+    }
+    public static class ExtensionSamples
+    {
+        //extension methods haet obe static and need to be in a static classs
+        // note: keyword this. -indiciates exnstion metthod.
+        // extending string, attaching own method to string
+        public static void PrintToConsole(this string message) {
+            Console.WriteLine(message);
+
+        }
+
+        //extension emthods, extending hotel room model 
+
+        public static HotalRoomModel TurnOnAir(this HotalRoomModel room)
+        {
+            room.IsAirRunning = true;
+            return room;
+        }
+        public static HotalRoomModel TurnOffAir(this HotalRoomModel room)
+        {
+            room.IsAirRunning = false;
+            return room;
+        }
+
+
+        public static HotalRoomModel SetTemparature(this HotalRoomModel room, int temperature)
+        {
+            room.Temperature = temperature;
+            return room;
+        }
+
+        public static HotalRoomModel OpenShades(this HotalRoomModel room)
+        {
+            room.AreShadesOpen = true;
+            return room;
+        }
+
+        public static HotalRoomModel CloseShades(this HotalRoomModel room)
+        {
+            room.AreShadesOpen = false;
+            return room;
+        }
+    }
+}
