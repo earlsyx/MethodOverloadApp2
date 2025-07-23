1a23f4db1184e6b0450af5c071f2ac46e09f4f7e
Earl Syx Hernandez <hernandezearlsyxmail@gmail.com>
2025-07-24 06:38:34 +0800

    HW

---
+++ HWExtensionMethods/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HWExtensionMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonModel person = new PersonModel();
+            person.SetDefaultAge().PrintInfo("Pokemon", "Kachu");
+            Console.ReadLine();
+        }
+    }
+
+    public class PersonModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+    }
+
+    public static class ExtensionMethods
+    {
+        public static PersonModel SetDefaultAge(this PersonModel person) 
+        {
+            person.Age = 18;
+            return person;
+        }
+
+        public static PersonModel PrintInfo(this PersonModel person,string firstName, string lastName)
+        {
+            person.FirstName = firstName;
+            person.LastName = lastName;
+            Console.WriteLine($"{firstName} {lastName} Age: {person.Age}");
+            return person;
+        }
+
+    }
+
+}
